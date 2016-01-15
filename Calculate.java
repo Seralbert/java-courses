9a03d0f0194490a47384a79052fd64f21f99f764
JS <a@a.a>
2016-01-15 12:48:29 +0300

    user input

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,7 @@
 public class Calculate {
 
 	public static void main(String[] args) {
-		System.out.println("Test");
+		System.out.println("Calculate...");
+		System.out.println(Integer.valueOf(args[0]) + Integer.valueOf(args[1]));
 	}
 }
